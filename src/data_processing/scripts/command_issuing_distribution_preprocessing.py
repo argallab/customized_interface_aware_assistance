6f257fc2a6060c54728c099ba0cb1824545856cf
mahdieh <m.nejati@u.northwestern.edu>
2020-02-13 13:56:43 -0600

    command processing, not complete

--- src/data_processing/scripts/command_issuing_distribution_preprocessing.py
+++ src/data_processing/scripts/command_issuing_distribution_preprocessing.py
@@ -7,6 +7,9 @@ import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt 
 import numpy as np
+from IPython import embed
+
+time_synch_difference = 0.01
 
 def build_parser(): 
 	# Builds the parser for reading the command line arguments 
@@ -15,21 +18,45 @@ def build_parser():
 	parser.add_argument('-command_prompt', help='name of /command_prompt file', nargs='*')
 	parser.add_argument('-input', help='name of /joy_sip_puff file', nargs='*')
 	parser.add_argument('-o', '--output', help='name of the output file', nargs='*')
+	parser.add_argument('-s', '--start', help='name of /keyboard_entry file', nargs='*')
 
 	return parser 
 
-def read_csv_files(path, command_prompt, user_input, output): 
+def read_csv_files(path, command_prompt, user_input, start, output): 
 	user_input_file = path + '/' + user_input
 	command_prompt_file = path + '/' + command_prompt
+	keyboard_entry_file = path + '/' + start
 	command_prompt_df = pd.read_csv(command_prompt_file, header = 0)
 	user_input_df = pd.read_csv(user_input_file, header = 0)
-	return command_prompt_df, user_input_df
+	keyboard_entry_df = pd.read_csv(keyboard_entry_file, header = 0)
+	return command_prompt_df, user_input_df, keyboard_entry_df
 
 # To do: computationally effecient way of doing this, instad of looping everytime
 def get_user_response_block(time_s, time_e, user_input): 
-	for i in range (len(user_input)): 
-		
+	time_diff = []
+	for i in range(len(user_input)): 
+		time_diff.append(user_input.at[i, 'rosbagTimestamp']  - time_s)
+		print time_diff[i]
+	embed()
+	print min(time_diff)
+	embed()
+		# print min(time_diff)
 
+def scale_times(command_prompt, user_input, start):
+
+	command_prompt_t = command_prompt.rosbagTimestamp
+	command_prompt_t = command_prompt_t.subtract(start.rosbagTimestamp)
+	command_prompt['rosbagTimestamp'] = command_prompt.rosbagTimestamp.replace(command_prompt_t)
+
+	# print user_input.rosbagTimestamp[0] 
+	a = user_input.rosbagTimestamp[0] 
+	user_input_t = user_input.rosbagTimestamp
+	user_input_t = user_input_t.subtract(start.rosbagTimestamp)
+	user_input['rosbagTimestamp'] = user_input.rosbagTimestamp.replace(user_input_t)
+
+	# print user_input.rosbagTimestamp[0] - a
+
+	return command_prompt, user_input
 
 def build_probabilities(command_prompt, user_input):
 	# To do: Change to 7 for separating deadbands
@@ -39,13 +66,13 @@ def build_probabilities(command_prompt, user_input):
 	u_ss = np.zeros(6)
 
 	COMMAND_TO_ARRAY_DICT = {'Hard Puff': u_hp, 'Hard Sip': u_hs, 'Soft Puff': u_sp, 'Soft Sip': u_ss}
-	for i in range(len(command_prompt)):
-		key =  command_prompt.get_value(i, 'command').replace('"','')
-		time_s =  command_prompt.get_value(i, 'rosbagTimestamp')
+	for i in range(30, len(command_prompt)):
+		key =  command_prompt.at[i, 'command'].replace('"','')
+		time_s =  command_prompt.at[i, 'rosbagTimestamp']
 		# To do: Change time end to the empty command
 		if i < len(command_prompt)-1: 
-			time_e = command_prompt.get_value(i+1, 'rosbagTimestamp')
-			print command_prompt.get_value(i+1, 'seq')
+			time_e = command_prompt.at[i+1, 'rosbagTimestamp']
+			# print command_prompt.get_value(i+1, 'seq')
 
 			get_user_response_block(time_s, time_e, user_input)
 
@@ -65,11 +92,19 @@ if __name__ == '__main__':
 	output = args.output
 	command_prompt = args.command_prompt[0]
 	user_input = args.input[0]
+	start = args.start[0]
 
-	topics = read_csv_files(path, command_prompt, user_input, output)
+	topics = read_csv_files(path, command_prompt, user_input, start, output)
 	# print topics[1].head()
 	# print topics[1][['axes']]
-	build_probabilities(topics[0], topics[1])
+	# build_probabilities(topics[0], topics[1], topics[2])
+	topics_scaled = scale_times(topics[0], topics[1], topics[2])
+	build_probabilities(topics_scaled[0], topics_scaled[1])
+
+
 	
 
+
+	# How to run: 
+	# python command_issuing_distribution_preprocessing.py -path mahdieh -command_prompt _slash_command_prompt.csv -input _slash_joy_sip_puff.csv -s _slash_keyboard_entry.csv
 
