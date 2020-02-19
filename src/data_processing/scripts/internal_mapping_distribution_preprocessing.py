75f666b2bd7bc96dd20cb6c7cc33a33e60ab4ded
mahdieh <m.nejati@u.northwestern.edu>
2020-02-19 16:24:35 -0600

    fixing internal_model_mapping distribution

--- src/data_processing/scripts/internal_mapping_distribution_preprocessing.py
+++ src/data_processing/scripts/internal_mapping_distribution_preprocessing.py
@@ -12,7 +12,7 @@ from IPython import embed
 import pickle  
 import itertools
 import collections 
-
+import bisect
 
 class DataParser(object): 
 	def __init__(self, file_dir):
@@ -35,12 +35,70 @@ class IntendedCommandGivenActionAnalysis(object):
 
 		self.file_dir = args.path
 		self.id = args.id
-		self.data_parser = DataParser(self.file_dir)
+		self.data = DataParser(self.file_dir)
+
+	def get_nearest_time_stamp(self, tq, time_stamp_array):
+		'''
+		assumes that time_stamp_array is a sorted (ascending) list.
+		returns: (time stamp closest to tq, index of the time stamp closest to tq in time_stamp_array)
+		'''
+		idx = bisect.bisect(time_stamp_array, tq) #idx is the index in the time_stamp_array whose value is immediately greater than tq
+		if idx == 0: #if requested timestamp is smaller than the first element of the list then return the first element of the list and the index is 0
+			return time_stamp_array[idx], idx
+		elif idx == len(time_stamp_array): #if tq is greater than the biggest timestamp in the list, then retirn the last timestamp and the index of len-1 or idx-1
+			return time_stamp_array[idx-1], idx-1
+		else:
+			prev_t = time_stamp_array[idx - 1]
+			next_t = time_stamp_array[idx]
+			if next_t - tq <= tq - prev_t: #if tq is closer to the next value
+				return time_stamp_array[idx], idx
+			else: #tq is closer to the previous value
+				return time_stamp_array[idx-1], idx-1
+
+
+	def get_user_response_block_indices(self, time_s, time_e, user_input):
+		assert 'rosbagTimestamp' in user_input
+		time_s_u, index_of_time_s_u = self.get_nearest_time_stamp(time_s, user_input.rosbagTimestamp)
+		time_e_u, index_of_time_e_u = self.get_nearest_time_stamp(time_e, user_input.rosbagTimestamp)
+
+		assert time_e_u == time_s_u #sanity checks
+		assert index_of_time_e_u == index_of_time_s_u #sanity check
+
+		user_response_block_indice = index_of_time_s_u
+
+		return user_response_block_indice
 		
 		
 	def build_distributions(self): 
-		pass 
 
+		USER_RESPONSE_DICT = {'1': 'Hard Puff', '2': 'Hard Sip', '3': 'Soft Puff', '4': 'Soft Sip'}
+		
+		# hard puff, hard sip, soft puff, soft sip
+		up = np.zeros(4)
+		down = np.zeros(4)
+		right = np.zeros(4)
+		left = np.zeros(4)
+		ccw = np.zeros(4)
+		cw = np.zeros(4)
+		mode_r_x = np.zeros(4)
+		mode_r_y = np.zeros(4)
+		mode_r_t = np.zeros(4)
+		mode_l_x = np.zeros(4)
+		mode_l_y = np.zeros(4)
+		mode_l_t = np.zeros(4)
+
+		ACTION_TO_ARRAY_DICT = {'up': up, 'down': down, 'left': left, 'right': right, 'clockwise': cw, 'counterclockwise': ccw, 
+								'mode_switch_right_1': mode_r_x, 'mode_switch_right_2': mode_r_y, 'mode_switch_right_3': mode_r_t, 
+								'mode_switch_left_1': mode_l_x, 'mode_switch_left_2': mode_l_y, 'mode_switch_left_3': mode_l_t}
+
+		for i in range(0, len(self.data.action_prompt_df)-1, 2): 
+
+			prompt_t_s = self.data.action_prompt_df.at[i, 'rosbagTimestamp']
+			prompt_t_e = self.data.action_prompt_df.at[i+2, 'rosbagTimestamp']
+
+			user_response_block_indices = self.get_user_response_block_indices(prompt_t_s, prompt_t_e, self.data.user_response_df)
+
+			embed()
 
 if __name__ == '__main__': 
 	parser = argparse.ArgumentParser()
@@ -51,4 +109,4 @@ if __name__ == '__main__':
 	puia = IntendedCommandGivenActionAnalysis(args)
 	puia.build_distributions()
 
-	# python command_issuing_distribution_preprocessing.py -path deepak -command_prompt _slash_command_prompt.csv -input _slash_joy_sip_puff.csv 
+	# python internal_mapping_distribution_preprocessing.py -path mahdieh_internal_model -id mahdieh
