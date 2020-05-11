9a360433ce57714c04cee14058dae107942b800c
Deepak Gopinath <musicmagic@gmail.com>
2020-05-11 09:47:20 -0500

    Added authors at the top of the data analysis files

--- src/data_analysis/scripts/p3_post_task_likert_analysis.py
+++ src/data_analysis/scripts/p3_post_task_likert_analysis.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
+# Code developed by Deepak Gopinath*, Mahdieh Nejati Javaremi* in February 2020. Copyright (c) 2020. Deepak Gopinath, Mahdieh Nejati Javaremi, Argallab. (*) Equal contribution
 import os
 import argparse
 import pandas as pd
 import numpy as np
-import pickle  
-from ast import literal_eval 
+import pickle
+from ast import literal_eval
 from IPython import embed
 import matplotlib
 import matplotlib.pyplot as plt
@@ -20,9 +21,9 @@ matplotlib.rcParams['ps.fonttype'] = 42
 class PostTaskLikertAnalysis(object):
 	def __init__(self, filename, *subject_id):
 
-		# get path to csv file of interest 
+		# get path to csv file of interest
 		self.filename = args.filename
-		# To DO: something about this path 
+		# To DO: something about this path
 		self.file_path = os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir, os.pardir)), 'data_processing/raw_data/qualtrics', self.filename)
 
 		assert os.path.exists(self.file_path)
@@ -43,8 +44,8 @@ class PostTaskLikertAnalysis(object):
 
 
 		self.question_text = self.df.loc[0, self.question_num] # first row is questions save as array
-		for i in range(len(self.question_text)): 
-			self.question_text[i] = self.question_text[i].split('Note')[0] 
+		for i in range(len(self.question_text)):
+			self.question_text[i] = self.question_text[i].split('Note')[0]
 
 		self.df = self.df.drop(0) # now remove first row from main dataframe so it's clearn
 		self.df.reset_index(drop=True, inplace=True) # now reset indices so starts from 0 again
@@ -52,23 +53,23 @@ class PostTaskLikertAnalysis(object):
 		if subject_id: # if looking at one subject
 			self.subject_id = subject_id[0]
 			self.df = self.df.loc[self.df['ID'] == self.subject_id]
-		else: 
+		else:
 			self.skip_ids() # skip test subjects
 
 		self.labels = ['Strongly Agree', 'Agree', 'Somewhat Agree', 'Neutral', 'Somewhat Disagree', 'Disagree', 'Strongly Disagree']
 		self.label_to_score = {'Strongly Agree': 3, 'Agree': 2, 'Somewhat Agree': 1, 'Neutral': 0, 'Somewhat Disagree': -1, 'Disagree': -2, 'Strongly Disagree': -3}
 		self.conditions= ['No Assistance', 'Filtered', 'Corrective']
-			   
+
 	def skip_ids(self):
-		# Id's to skip (test id's, manual cleaning) 
+		# Id's to skip (test id's, manual cleaning)
 		# To do: instead of hardcode add input argument
 		ids = ['dan', 'deepak', 'andrew']
-		for i in range(len(ids)): 
+		for i in range(len(ids)):
 			self.df = self.df[self.df.ID != ids[i]]
 		self.df.reset_index(drop=True, inplace=True)
-		
 
-	def get_percentages(self, data): 
+
+	def get_percentages(self, data):
 
 		data.reset_index(drop=True, inplace=True)
 
@@ -76,33 +77,33 @@ class PostTaskLikertAnalysis(object):
 		# percentes = [question1, question2, etc] where qeustion1=[%(strongly agree), %(agree), %(somewhat agree), %(neutral), etc]
 		percentages = []
 
-		
+
 		total_resondants = float(len(data)) # total number of people who repsonded to quesitonnaire
 
-		for i in self.question_num: 
+		for i in self.question_num:
 			q_responses = list()
-			for j in self.labels: 
+			for j in self.labels:
 				reponse_percent = 100.0*len(data[data[i]==j].index.tolist())/total_resondants
 				q_responses.append(reponse_percent)
 			percentages.append(q_responses)
 		return percentages
 
 
-	def get_ranks_to_score(self, data): 
+	def get_ranks_to_score(self, data):
 
 		data.reset_index(drop=True, inplace=True)
 
 		# an array for each quesitons containing the label-to-score value of each respondant's response to that quesiton
 		scores = []
-		for i in self.question_num: 
+		for i in self.question_num:
 			q_score = list()
-			for j in range(len(data)): 
+			for j in range(len(data)):
 				q_score.append(self.label_to_score[data.loc[j, i]])
 			scores.append(q_score)
 		return scores
 
 
-	def plot_mean_rank_horizontal_bar_plot(self, responses, num_resp, title): 
+	def plot_mean_rank_horizontal_bar_plot(self, responses, num_resp, title):
 
 		plt.rcdefaults()
 		fig, ax = plt.subplots()
@@ -126,38 +127,38 @@ class PostTaskLikertAnalysis(object):
 		plt.show()
 
 
-	def plot_stacked_horizontal_bar_plot(self, df): 
+	def plot_stacked_horizontal_bar_plot(self, df):
 
-		
+
 		sns.set_style("dark")
 		sns.set_context("paper")
 		sns.set_palette("colorblind")
 
-		ax = sns.barplot(x='score', y='question', hue='condition', data=df)  
+		ax = sns.barplot(x='score', y='question', hue='condition', data=df)
 
 
 		font_size = 20
 		ax.tick_params(labelsize='xx-large')
 
 
-		# TO DO: use gloabl var		
+		# TO DO: use gloabl var
 		ax.set_xticklabels(['Strongly Disagree', 'Disagree', 'Neutral', 'Somewhat Agree', 'Agree', 'Strongly Agree'])
 		plt.xlabel('')
 		plt.ylabel('')
 		plt.subplots_adjust(left=.49)
 		plt.subplots_adjust(right=.97)
 		plt.setp(ax.get_legend().get_texts(), fontsize=font_size) # for legend text
-		plt.show()  
+		plt.show()
 
 
-	def group_per_assitance_condition(self): 
+	def group_per_assitance_condition(self):
 
 		# To do: instead of this get Assistance_Type column index levels like R or make key with self.condition
 		assistance_condition = ['Noas', 'Filtas', 'Coras']
 
 		df = pd.DataFrame(columns=['score', 'question', 'condition'])
 
-		for i, cond in enumerate(assistance_condition): 
+		for i, cond in enumerate(assistance_condition):
 			sub_df = self.df[self.df['Assistance_Type'] == cond]
 			responses = self.get_ranks_to_score(sub_df)
 			question = []
@@ -181,11 +182,10 @@ if __name__ == '__main__':
 	parser.add_argument('-f', '--filename', help='qaultrics post-task survey', default='post_task_survey.csv', type=str) # has defualt,
 	parser.add_argument('-id', '--subject_id', help='experiment block: subject_id_type_assistance_block', type=str) # no default but optional
 	args = parser.parse_args()
-	if args.subject_id: 
+	if args.subject_id:
 		likert = PostTaskLikertAnalysis(args.filename, args.subject_id)
-	else: 
+	else:
 		likert = PostTaskLikertAnalysis(args.filename)
-	
+
 	likert.group_per_assitance_condition()
 	# likert.plot_percentage_bar_plot()
-	
--- src/data_analysis/scripts/post_session_ranking_analysis.py
+++ src/data_analysis/scripts/post_session_ranking_analysis.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
+# Code developed by Deepak Gopinath*, Mahdieh Nejati Javaremi* in February 2020. Copyright (c) 2020. Deepak Gopinath, Mahdieh Nejati Javaremi, Argallab. (*) Equal contribution
 import os
 import argparse
 import pandas as pd
 import numpy as np
-import pickle  
-from ast import literal_eval 
+import pickle
+from ast import literal_eval
 from IPython import embed
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
@@ -21,16 +22,16 @@ import matplotlib.colors as mcolors
 class PostSessionRankingSurvey(object):
     def __init__(self, args):
 
-        # get path to csv file of interest 
+        # get path to csv file of interest
         self.filename = args.filename
-        # To DO: something about this path 
+        # To DO: something about this path
         self.block_dir = os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir, os.pardir)), 'data_processing/raw_data/qualtrics', self.filename)
 
         assert os.path.exists(self.block_dir)
 
         # only columns of interest
-        columns = ['Progress', 'Duration (in seconds)', 'Finished', 'RecordedDate', 'ID', 'Preference_1', 'Preference_2', 'Preference_3', 
-                    'Difficulty_1', 'Difficulty_2', 'Difficulty_3', 'Understandability_1', 'Understandability_2', 'Understandability_3', 
+        columns = ['Progress', 'Duration (in seconds)', 'Finished', 'RecordedDate', 'ID', 'Preference_1', 'Preference_2', 'Preference_3',
+                    'Difficulty_1', 'Difficulty_2', 'Difficulty_3', 'Understandability_1', 'Understandability_2', 'Understandability_3',
                     'Helpfulness_1', 'Helpfulness_2', 'Helpfulness_3']
 
         # read csv file ask dataframe
@@ -39,14 +40,14 @@ class PostSessionRankingSurvey(object):
         self.skip_ids() # skip test subjects
 
     def skip_ids(self):
-        # Id's to skip (test id's, manual cleaning) 
+        # Id's to skip (test id's, manual cleaning)
         # To do: instead of hardcode add input argument
         ids = ['dan', 'deepak', 'andrew']
-        for i in range(len(ids)): 
+        for i in range(len(ids)):
             self.df = self.df[self.df.ID != ids[i]]
         self.df.reset_index(drop=True, inplace=True)
 
-    def get_response_per_category(self): 
+    def get_response_per_category(self):
         # array: [0, 1, 2] TO DO: Change to [1, 2, 3]
 
         first_rank = 1  # First
@@ -57,58 +58,58 @@ class PostSessionRankingSurvey(object):
         self.preference_noas = [len(self.df[self.df.Preference_1 == first_rank].index.tolist()),    # First
                                 len(self.df[self.df.Preference_1 == second_rank].index.tolist()),   #Second
                                 len(self.df[self.df.Preference_1 == third_rank].index.tolist())]    #Third
-        self.preference_filtas = [len(self.df[self.df.Preference_2 == first_rank].index.tolist()), 
-                                len(self.df[self.df.Preference_2 == second_rank].index.tolist()), 
-                                len(self.df[self.df.Preference_2 == third_rank].index.tolist())] 
-        self.preference_coras = [len(self.df[self.df.Preference_3 == first_rank].index.tolist()), 
-                                len(self.df[self.df.Preference_3 == second_rank].index.tolist()), 
-                                len(self.df[self.df.Preference_3 == third_rank].index.tolist())] 
-        
+        self.preference_filtas = [len(self.df[self.df.Preference_2 == first_rank].index.tolist()),
+                                len(self.df[self.df.Preference_2 == second_rank].index.tolist()),
+                                len(self.df[self.df.Preference_2 == third_rank].index.tolist())]
+        self.preference_coras = [len(self.df[self.df.Preference_3 == first_rank].index.tolist()),
+                                len(self.df[self.df.Preference_3 == second_rank].index.tolist()),
+                                len(self.df[self.df.Preference_3 == third_rank].index.tolist())]
 
-        self.difficulty_noas = [len(self.df[self.df.Difficulty_1 == first_rank].index.tolist()), 
-                                len(self.df[self.df.Difficulty_1 == second_rank].index.tolist()), 
-                                len(self.df[self.df.Difficulty_1 == third_rank].index.tolist())] 
-        self.difficulty_filtas = [len(self.df[self.df.Difficulty_2 == first_rank].index.tolist()), 
-                                len(self.df[self.df.Difficulty_2 == second_rank].index.tolist()), 
-                                len(self.df[self.df.Difficulty_2 == third_rank].index.tolist())] 
-        self.difficulty_coras = [len(self.df[self.df.Difficulty_3 == first_rank].index.tolist()), 
-                                len(self.df[self.df.Difficulty_3 == second_rank].index.tolist()), 
-                                len(self.df[self.df.Difficulty_3 == third_rank].index.tolist())] 
 
-        
-        self.understandability_noas = [len(self.df[self.df.Understandability_1 == first_rank].index.tolist()), 
-                                        len(self.df[self.df.Understandability_1 == second_rank].index.tolist()), 
+        self.difficulty_noas = [len(self.df[self.df.Difficulty_1 == first_rank].index.tolist()),
+                                len(self.df[self.df.Difficulty_1 == second_rank].index.tolist()),
+                                len(self.df[self.df.Difficulty_1 == third_rank].index.tolist())]
+        self.difficulty_filtas = [len(self.df[self.df.Difficulty_2 == first_rank].index.tolist()),
+                                len(self.df[self.df.Difficulty_2 == second_rank].index.tolist()),
+                                len(self.df[self.df.Difficulty_2 == third_rank].index.tolist())]
+        self.difficulty_coras = [len(self.df[self.df.Difficulty_3 == first_rank].index.tolist()),
+                                len(self.df[self.df.Difficulty_3 == second_rank].index.tolist()),
+                                len(self.df[self.df.Difficulty_3 == third_rank].index.tolist())]
+
+
+        self.understandability_noas = [len(self.df[self.df.Understandability_1 == first_rank].index.tolist()),
+                                        len(self.df[self.df.Understandability_1 == second_rank].index.tolist()),
                                         len(self.df[self.df.Understandability_1 == third_rank].index.tolist())]
-        self.understandability_filtas = [len(self.df[self.df.Understandability_2 == first_rank].index.tolist()), 
-                                        len(self.df[self.df.Understandability_2 == second_rank].index.tolist()), 
-                                        len(self.df[self.df.Understandability_2 == third_rank].index.tolist())] 
-        self.understandability_coras = [len(self.df[self.df.Understandability_3 == first_rank].index.tolist()), 
-                                        len(self.df[self.df.Understandability_3 == second_rank].index.tolist()), 
-                                        len(self.df[self.df.Understandability_3 == third_rank].index.tolist())] 
+        self.understandability_filtas = [len(self.df[self.df.Understandability_2 == first_rank].index.tolist()),
+                                        len(self.df[self.df.Understandability_2 == second_rank].index.tolist()),
+                                        len(self.df[self.df.Understandability_2 == third_rank].index.tolist())]
+        self.understandability_coras = [len(self.df[self.df.Understandability_3 == first_rank].index.tolist()),
+                                        len(self.df[self.df.Understandability_3 == second_rank].index.tolist()),
+                                        len(self.df[self.df.Understandability_3 == third_rank].index.tolist())]
 
 
-        self.helpfulness_noas = [len(self.df[self.df.Helpfulness_1 == first_rank].index.tolist()), 
-                                len(self.df[self.df.Helpfulness_1 == second_rank].index.tolist()), 
-                                len(self.df[self.df.Helpfulness_1 == third_rank].index.tolist())] 
-        self.helpfulness_filtas = [len(self.df[self.df.Helpfulness_2 == first_rank].index.tolist()), 
-                                len(self.df[self.df.Helpfulness_2 == second_rank].index.tolist()), 
-                                len(self.df[self.df.Helpfulness_2 == third_rank].index.tolist())] 
-        self.helpfulness_coras = [len(self.df[self.df.Helpfulness_3 == first_rank].index.tolist()), 
-                                len(self.df[self.df.Helpfulness_3 == second_rank].index.tolist()), 
-                                len(self.df[self.df.Helpfulness_3 == third_rank].index.tolist())]                         
+        self.helpfulness_noas = [len(self.df[self.df.Helpfulness_1 == first_rank].index.tolist()),
+                                len(self.df[self.df.Helpfulness_1 == second_rank].index.tolist()),
+                                len(self.df[self.df.Helpfulness_1 == third_rank].index.tolist())]
+        self.helpfulness_filtas = [len(self.df[self.df.Helpfulness_2 == first_rank].index.tolist()),
+                                len(self.df[self.df.Helpfulness_2 == second_rank].index.tolist()),
+                                len(self.df[self.df.Helpfulness_2 == third_rank].index.tolist())]
+        self.helpfulness_coras = [len(self.df[self.df.Helpfulness_3 == first_rank].index.tolist()),
+                                len(self.df[self.df.Helpfulness_3 == second_rank].index.tolist()),
+                                len(self.df[self.df.Helpfulness_3 == third_rank].index.tolist())]
 
-        self.data = np.concatenate((self.preference_noas, self.preference_filtas, self.preference_coras, 
-                                self.difficulty_noas, self.difficulty_filtas, self.difficulty_coras, 
-                                self.understandability_noas, self.understandability_filtas, self.understandability_coras, 
+        self.data = np.concatenate((self.preference_noas, self.preference_filtas, self.preference_coras,
+                                self.difficulty_noas, self.difficulty_filtas, self.difficulty_coras,
+                                self.understandability_noas, self.understandability_filtas, self.understandability_coras,
                                 self.helpfulness_noas, self.helpfulness_filtas, self.helpfulness_coras), axis=0)
 
-    def plot_ranks_together(self): 
+    def plot_ranks_together(self):
         # TO DO: Cleaner way to do this! Yikes
         x = [.5, .5, .5,  1, 1, 1, 1.5, 1.5, 1.5 , 2.5, 2.5, 2.5 , 3, 3, 3, 3.5, 3.5, 3.5, 4.5, 4.5, 4.5, 5, 5, 5, 5.5, 5.5, 5.5, 6.5, 6.5, 6.5, 7, 7, 7, 7.5, 7.5, 7.5]
         # y is 1st, 2nd, 3rd rank
-             
+
         y = []
-        for i in range(0, len(x), 3): 
+        for i in range(0, len(x), 3):
             y.append(3)      # First
             y.append(2)      # Second
             y.append(1)      # Third
@@ -118,12 +119,12 @@ class PostSessionRankingSurvey(object):
         coras_color = 'tab:green'
 
         colors = [None]*len(x)
-        for i in range(0, len(x), 9): 
-            for j in range(3): 
+        for i in range(0, len(x), 9):
+            for j in range(3):
                 colors[i+j] = noas_color
-            for j in range(3, 6): 
+            for j in range(3, 6):
                 colors[i+j] = filtas_color
-            for j in range(6, 9): 
+            for j in range(6, 9):
                 colors[i+j] = coras_color
 
         xtick_loc = [1, 3, 5, 7]
@@ -142,7 +143,7 @@ class PostSessionRankingSurvey(object):
         plt.title('Post Session Ranking')
         plt.legend(handles=[noas_patch, filtas_patch, coras_patch])
         plt.show()
-            
+
         # scatter plot with weighted area
 
 
