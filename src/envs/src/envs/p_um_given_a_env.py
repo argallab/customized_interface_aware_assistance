83d12ceec8530ea0978ab5588a4479cd70ac161d
Deepak Gopinath <musicmagic@gmail.com>
2021-03-24 14:39:33 -0500

    fixed imports. Recreated trials. Added test blocks

--- src/envs/src/envs/p_um_given_a_env.py
+++ src/envs/src/envs/p_um_given_a_env.py
@@ -3,6 +3,9 @@
 from Box2D import (b2EdgeShape, b2FixtureDef, b2PolygonShape, b2Random, b2CircleShape, b2Vec2, b2Color)
 import Box2D
 from backends.rendering import Viewer, Transform, LineWidth
+import sys
+import os
+import rospkg
 sys.path.append(os.path.join(rospkg.RosPack().get_path('simulators'), 'scripts'))
 from corrective_mode_switch_utils import *
 import csv
